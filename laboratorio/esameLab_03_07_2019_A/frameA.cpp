4f5e10afe0e88dedd798f90105e41b7460579188
Francisco Pablo Russo <franciscopablorusso@gmail.com>
2021-09-30 23:14:03 +0200

    +

--- laboratorio/esameLab_03_07_2019_A/frameA.cpp
+++
@@ -1,9 +0,0 @@
-srand(111222333);     
-
-	for(int i=0; i<DIM; i++){
-		short n=1+rand()%10;
-		if(rand()%2==0)
-	  		vett[i]= new B(n, rand()%5+1, rand()%11+10, (double) rand()/(RAND_MAX));
-		else
-	  		vett[i]= new C(n, (char) (rand()%('z' - 'a' + 1) + 'a'), rand()%5+1, rand()%11+10);
-   	}
